4c18956099934bf85de74bfb2a02068f32cea92b
Diane L. Jones <dlj.co27@gmail.com>
2019-05-26 21:59:07 -0400

    Removed body-parser.  Body-Parser has been deprecated.

--- tracker-app/backend/server.js
+++ tracker-app/backend/server.js
@@ -1,6 +1,6 @@
 // Dependencies
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser'); deprecated
 const cors = require('cors');
 const mongoose = require('mongoose');
 
@@ -10,7 +10,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.json()); // REsplaced deprecated bodyParser
 // Database Connect
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
